2332af37452b96f7b861b37bf892d4d3cc8190a7
Bernadette St. George <bernadette.stgeorge@gmail.com>
2019-01-24 17:48:26 -0700

    Update chapter-parser.js

--- chapter-parser.js
+++ chapter-parser.js
@@ -5,7 +5,7 @@
             "V0VCVlRUCgowMDowMDowMC4wMDAgLS0+IDAwOjAwOjMwLjAwMApEaXNjbGFpbWVycwoKMDA6MDA6MzAuMDAwIC0tPiAwMDowMToyOC4wMDAKUHJvZ3JhbSBTdGFydAoKMDA6MDE6MjguMDAwIC0tPiAwMDowMjowNy4wMDAKSW50cm9kdWN0aW9uCgowMDowMjowNy4wMDAgLS0+IDAwOjAzOjQ5LjAwMApBbmF0b215IG9mIFByZWduYW5jeQoKMDA6MDM6NDkuMDAwIC0tPiAwMDowODoyMi4wMDAKUHJlLUxhYm9yIFNpZ25zCgowMDowODoyMi4wMDAgLS0+IDAwOjEwOjUxLjAwMApPbnNldCBvZiBMYWJvcgoKMDA6MTA6NTEuMDAwIC0tPiAwMDoxMTo1Ny4wMDAKTGFib3IgT3ZlcnZpZXcKCjAwOjExOjU3LjAwMCAtLT4gMDA6MTU6MzcuMDAwCkZpcnN0IFN0YWdlCgowMDoxNTozNy4wMDAgLS0+IDAwOjIwOjAyLjAwMApBY3RpdmUgTGFib3IKCjAwOjIwOjAyLjAwMCAtLT4gMDA6MjI6NDcuMDAwClRyYW5zaXRpb24KCjAwOjIyOjQ3LjAwMCAtLT4gMDA6Mjg6NTcuMDAwClNlY29uZCBTdGFnZQoKMDA6Mjg6NTcuMDAwIC0tPiAwMDozMTowOC4wMDAKVGhpcmQgU3RhZ2UKCjAwOjMxOjA4LjAwMCAtLT4gMDA6MzU6MDIuMDAwCkZvdXJ0aCBTdGFnZQoKMDA6MzU6MDIuMDAwIC0tPiAwMDozNzoxMS4yMDAKQ3JlZGl0cwoKMDA6Mzc6MTEuMjAwIC0tPiAwMDozODoxMS4yMDAKQ29udGFjdCBJbmZvcm1hdGlvbg=="
         );
 
-        //functional parser for selecting times and converting the times into seconds
+        //converts times (for each video section) into seconds
         secondsArr = [];
         function timesToSeconds(str) {
             var newTime;
